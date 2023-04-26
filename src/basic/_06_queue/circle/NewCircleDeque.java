3ce0f6b4a963e0c2f23c2d69f58a3da11a99b699
yujale98 <yujale98@gmail.com>
2023-04-26 17:15:10 +0800

    feat:新增数据结构队列之循环队列

--- src/basic/_06_queue/circle/NewCircleDeque.java
+++ src/basic/_06_queue/circle/NewCircleDeque.java
@@ -4,6 +4,147 @@ package basic._06_queue.circle;
 /**
  * 循环双端队列
  */
-public class NewCircleDeque {
+public class NewCircleDeque<E> {
+
+  private int front; // 队头指针
+  private int size; // 元素数量
+  private Object elements[]; // 元素
+  public static final int DEFAULT_CAPACITY = 10; // 初始容量
+
+  public NewCircleDeque() {
+    elements = new Object[DEFAULT_CAPACITY];
+  }
+
+  /**
+   * 获取队列中元素的个数
+   *
+   * @return 队列中元素的个数
+   */
+  public int size() {
+    return size;
+  }
+
+  /**
+   * 是否为空
+   */
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  /**
+   * 清空
+   */
+  public void clear() {
+    for (int i = 0; i < size; i++) {
+      elements[index(i)] = null;
+    }
+    front = 0;
+    size = 0;
+  }
+
+  /**
+   * 从队尾入队
+   */
+  public void enQueueRear(E element) {
+    // 头 1 r(2) null null null f(6) 7 8 9 尾
+    ensureCapacity(size + 1);
+
+    elements[index(size)] = element;
+    size++;
+  }
+
+  /**
+   * 从队头入队
+   */
+  public void enQueueFront(E element) {
+    ensureCapacity(size + 1);
+
+    front = index(-1);
+    elements[front] = element;
+    size++;
+  }
+
+  /**
+   * 从队尾出队
+   */
+  public E deQueueRear() {
+    int rearIndex = index(size - 1);
+    E rear = (E) elements[rearIndex];
+    elements[rearIndex] = null;
+    size--;
+    return rear;
+  }
+
+  /**
+   * 从队头出队
+   */
+  // 头 1 r(2) null null f(5) 6 7 8 9 尾
+  public E deQueueFront() {
+    E frontElement = (E) elements[front];
+    elements[front] = null;
+    front = index(1);
+    size--;
+    return frontElement;
+  }
+
+  /**
+   * 获取队列的头元素
+   */
+  public E front() {
+    return (E) elements[front];
+  }
+
+  /**
+   * 获取队列的尾元素
+   */
+  public E rear() {
+    return (E) elements[index(size - 1)];
+  }
+
+  // 索引封装映射
+  private int index(int index) {
+    index += front;
+    if (index < 0) { // index 为负数
+      return index + elements.length;
+    }
+    // index 为正数
+    return index % elements.length;
+  }
+
+  // 数组扩容
+  private void ensureCapacity(int capacity) {
+    int oldCapacity = elements.length;
+    if (oldCapacity >= capacity) {
+      return;
+    }
+
+    int newCapacity = oldCapacity + (oldCapacity >> 1); // 扩容为1.5倍
+    E newElements[] = (E[]) new Object[newCapacity];
+    for (int i = 0; i < size; i++) {
+      newElements[i] = (E) elements[index(i)];
+    }
+    elements = newElements;
+    front = 0; // 重置front
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder string = new StringBuilder();
+    string.append("capacity=").append(elements.length).append(" size=").append(size)
+        .append(" front=")
+        .append(front).append(", [");
+    return getString(string, elements);
+  }
+
+  static String getString(StringBuilder string, Object[] elements) {
+    for (int i = 0; i < elements.length; i++) {
+      if (i != 0) {
+        string.append(", ");
+      }
+      string.append(elements[i]);
+    }
+    string.append("]");
+    return string.toString();
+  }
 
 }
--- src/basic/_06_queue/circle/NewCircleQueue.java
+++ src/basic/_06_queue/circle/NewCircleQueue.java
@@ -7,34 +7,66 @@ package basic._06_queue.circle;
  */
 public class NewCircleQueue<E> {
 
+  /**
+   * 队头元素的索引
+   */
   private int front;
 
+  /**
+   * 队列中元素的个数
+   */
   private int size;
-
+  /**
+   * 队列
+   */
   private Object[] elements;
 
+  /**
+   * 队列默认的容量
+   */
   private static final int DEFAULT_CAPACITY = 10;
 
   private static final int MAX_CAPACITY = Integer.MAX_VALUE - 8;
 
+  /**
+   * 构造函数
+   */
   public NewCircleQueue() {
     this(DEFAULT_CAPACITY);
   }
 
+  /**
+   * 构造函数
+   *
+   * @param capacity 队列的容量
+   */
   public NewCircleQueue(int capacity) {
     capacity = Math.max(capacity, DEFAULT_CAPACITY);
     elements = new Object[capacity];
   }
 
 
+  /**
+   * 获取队列中元素的个数
+   *
+   * @return 队列中元素的个数
+   */
   public int size() {
     return size;
   }
 
+  /**
+   * 队列是否为空
+   *
+   * @return true为空 false不为空
+   */
   public boolean isEmpty() {
     return size == 0;
   }
 
+  /**
+   * 清空队列
+   */
   public void clear() {
     for (int i = 0; i < size; i++) {
       elements[index(i)] = null;
@@ -43,11 +75,22 @@ public class NewCircleQueue<E> {
     front = 0;
   }
 
+  /**
+   * 获取index位置对应的真实索引
+   *
+   * @param index 索引
+   * @return 真实索引
+   */
   public int index(int index) {
     return (front + index) % elements.length;
   }
 
 
+  /**
+   * 出队
+   *
+   * @return 出队的元素
+   */
   public E deQueue() {
     E element = (E) elements[front];
     elements[front] = null;
@@ -56,6 +99,11 @@ public class NewCircleQueue<E> {
     return element;
   }
 
+  /**
+   * 入队
+   *
+   * @param element 入队的元素
+   */
   public void enQueue(E element) {
     ensureCapacity(size + 1);
     elements[index(size)] = element;
@@ -69,6 +117,11 @@ public class NewCircleQueue<E> {
     return elements[front];
   }
 
+  /**
+   * 扩容
+   *
+   * @param capacity 队列的容量
+   */
   private void ensureCapacity(int capacity) {
     int oldCapacity = elements.length;
     if (oldCapacity >= capacity) {
@@ -92,9 +145,13 @@ public class NewCircleQueue<E> {
   @Override
   public String toString() {
     StringBuilder string = new StringBuilder();
-    string.append("capcacity = ").append(elements.length).append(" size=").append(size)
+    string.append("capacity = ").append(elements.length).append(" size=").append(size)
         .append(" front=")
         .append(front).append(", [");
+    return getString(string, elements);
+  }
+
+  static String getString(StringBuilder string, Object[] elements) {
     for (int i = 0; i < elements.length; i++) {
       if (i != 0) {
         string.append(", ");
@@ -104,5 +161,4 @@ public class NewCircleQueue<E> {
     string.append("]");
     return string.toString();
   }
-
 }
